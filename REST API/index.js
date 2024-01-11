a9e08b10a7e97280fe94dcdb59408e90bd3fb3cb
Sayantan Paul <sayantan.paul9748@gmail.com>
2024-01-12 01:39:08 +0530

    get request updated

--- REST API/index.js
+++ REST API/index.js
@@ -1,10 +1,28 @@
 const express=require('express')
-
+const userData=require('./MOCK_DATA.json')
 const app=express()
 const PORT=8001
 
-app.get('/', (req, res)=>{
-    res.end("root address activated")
+app.get('/api/users', (req, res)=>{
+    return res.json(userData)
+})
+
+app.route('/api/users/:id')
+.get((req, res)=>{
+    const id=Number(req.params.id)
+    const user=userData.find(user=>user.id===id)
+    return res.json(user)
+})
+.post((req, res)=>{ 
+    return res.json({status: "to be completed"})
+})
+
+.patch((req, res)=>{ 
+    return res.json({status: "to be completed"})
+})
+
+.delete((req, res)=>{ 
+    return res.json({status: "to be completed"})
 })
 
 
