ce1c7745f8092d5bc868c4ad162cc6f9b5309c3c
Sayantan Paul <sayantan.paul9748@gmail.com>
2024-01-15 12:20:22 +0530

    mongodb schemas and connections

--- REST API/index.js
+++ REST API/index.js
@@ -1,14 +1,46 @@
 const express=require('express')
-const userData=require('./MOCK_DATA.json')
+const mongoose=require("mongoose")
 const fs=require('fs')
 const app=express()
 const PORT=8001
+
+//connect to the database
+
+mongoose.connect('mongodb://127.0.0.1:27017/sampleUsers')
+.then(console.log("database connected"))
+.catch((err)=>console.log("error occured", err))
+
+//schemas
+const userSchema=new mongoose.Schema({
+    first_name:{
+        type: String,
+        require: true
+    },
+    last_name:{
+        type: String,
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    gender:{
+        type: String,
+    },
+    job_title: {
+        type: String,
+        require: true
+    },  
+}, {
+    timestamps: true
+})
+
+const UserModel=mongoose.model("userDetails",userSchema)
 
 //middleware
 app.use(express.json())
 
 //custom middleware
-
 app.use((req, res, next)=>{
     fs.appendFile('log.txt', `\naccessed at ${Date.now()}: on ${req.url} route and used ${req.method} method`,(err, data)=>{
         next()
@@ -17,27 +49,36 @@ app.use((req, res, next)=>{
 })
 
 //get the all user info in this route
-app.get('/api/users', (req, res)=>{
+app.get('/api/users', async (req, res)=>{
     res.setHeader('X-Dyson', 'Sayantan Paul') //custom header
     //always add X to custom headers- good practice 
-    return res.json(userData)
+
+    const allDBusers=await UserModel.find({})
+    return res.json(allDBusers)
 })
 
 //add new user via post req
-app.post('/api/users', (req, res)=>{
+app.post('/api/users', async (req, res)=>{
     const body=req.body
-    userData.push({...body, id: userData.length+1});
+    if(!body || !body.first_name ||!body.last_name ||!body.email|| !body.gender ||!body.job_title) return res.status(400).json({status: "kindly fill all the queries"})
     //update the json file
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(userData), (err, data)=>{
-        res.status(202).json({status: 'your id has been generated', id: userData.length })
+    const userform=await UserModel.create({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        job_title: body.job_title
     })
+    console.log(userform);
+    return res.status(200).json({status: "form submitted "})
+
 })
 
 //get the individual user via matching user id
 app.route('/api/users/:id')
-.get((req, res)=>{
-    const id=Number(req.params.id)
-    const user=userData.find(user=>user.id===id)
+.get( async(req, res)=>{
+
+    const user=await UserModel.findById(req.params.id)
     if(user){
         return res.json(user)
     }
@@ -45,30 +86,21 @@ app.route('/api/users/:id')
 })
 
 // update data via patch request
-.patch((req, res)=>{  
-    const id=Number(req.params.id)
-    const user=userData.find(user=>user.id===id)
-    if(user){
-        Object.assign(user, req.body)
-        fs.writeFile('./MOCK_DATA.json', JSON.stringify(userData), (err, data)=>{
-            return res.json({status: 'your data has been updated', id: user.id })
-        })
-    }else  return res.json({err: `error occoured`})
+.patch(async(req, res)=>{  
+    const body=req.body
+    const user=await UserModel.findByIdAndUpdate(req.params.id, {
+        first_name: body.first_name,
+        last_name: body.last_name,
+        job_title: body.job_title
+    })
+    return res.json({status: 'your data has been updated', id: user.id })
 })
 
 // delete data via delete request
-.delete((req, res)=>{ 
-    const id=Number(req.params.id)
-    const userIndex=userData.findIndex(user=>user.id===id)
-    if(userIndex!==-1){
-        userData.splice(userIndex, 1)
-        fs.writeFile('./MOCK_DATA.json', JSON.stringify(userData), (err, data)=>{
-            return res.json({status: 'your data has been deleted', id: id})
-        })
-    }
-    else return res.json({status: "not found"})
+.delete(async(req, res)=>{ 
+    await UserModel.findByIdAndDelete((req.params.id))
+    return res.json({status: 'your data has been deleted', id: id})
 })
-
 
 app.listen(PORT, ()=>{  
     console.log(`server active on port: ${PORT}`);
